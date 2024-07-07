bb1c0ca4ba442ab1d8b8cc59c8ca1548ce7c0c03
Nikolay Gubankov <nikgub@cock.li>
2024-07-07 20:15:53 +0300

    Updated permissions

---
+++ colm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+user=`whoami`
+config_dir="$COLM"
+[ -n $config_dir ] && config_dir=.config/colm
+config_file=/home/$user/$config_dir/config
+if [[ ! -d /home/$user/$config_dir ]]; then
+  mkdir /home/$user/$config_dir
+  echo "Configuration file went missing. Generated an empty one."
+  echo "lists_dir /home/$user/.colmlists" >> $config_file
+fi
+echo "$config_file"
+
+lists_dir=`grep "lists_dir" $config_file | awk '{print $2}'`
+list_name="$user-list"
+entry_name=""
+
+while getopts 'C:L:a:' key; do
+  case "$key" in
+    C) echo 1
+    ;;
+    L) list_name=$OPTARG
+    ;;
+    a) entry_name=$OPTARG
+    ;;
+  esac
+done
+if (echo "$list_name" | grep -xq "\-.*"); then
+  echo "Must pass an option to an -L key"
+  exit 1
+fi
+[ ! -n "$entry_name" ] && exit 2
+[ ! -n "$list_name" ] && exit 2
+[ ! -f "$lists_dir/$list_name" ] && touch "$lists_dir/$list_name"
+echo "$lists_dir/$list_name"
+echo "$entry_name" >> $lists_dir/$list_name
